597fe205159ac3a840ccf64a6652d687c0b34909
sarumathiramasamy <43061815+sarumathiramasamy@users.noreply.github.com>
2020-06-07 23:55:32 +0800

    Updates

---
+++ precache-manifest.1014fa78cee6748d140db18becec9b65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b086e63ea4fd47fdc096a2c120ef587",
+    "url": "/Tourism/index.html"
+  },
+  {
+    "revision": "a3083380c27e9e6420c6",
+    "url": "/Tourism/static/css/main.1c64cbb4.chunk.css"
+  },
+  {
+    "revision": "1a02c03e4ae393b98931",
+    "url": "/Tourism/static/js/2.995ffaac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Tourism/static/js/2.995ffaac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3083380c27e9e6420c6",
+    "url": "/Tourism/static/js/main.9c031002.chunk.js"
+  },
+  {
+    "revision": "2a5a788d99f61ba006a0",
+    "url": "/Tourism/static/js/runtime-main.05ec78f9.js"
+  }
+]);
